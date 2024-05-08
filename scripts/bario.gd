9c756d2d14b750262d5df851efcf656450ac1eed
Tylefnx <tayfunucuncu@gmail.com>
2024-05-08 17:39:33 +0300

    Updated player speed etc

--- scripts/bario.gd
+++ scripts/bario.gd
@@ -1,30 +1,41 @@
 extends CharacterBody2D
 
 
-const SPEED = 150.0
+const SPEED = 100
 const JUMP_VELOCITY = -300.0
 @onready var animation = $animated_sprite
-
+var dir = "Right"
+@onready var walk_timer = $move_timer
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
-func _physics_process(delta):
-	# Add the gravity.
-	animation.play("idleRight")	
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+###### FUNDAMENTAL MECHANICS ######
+func update_animation():
+	#walk_timer.start()
+	if velocity:
+		if velocity.x > 0: dir = "Right"
+		elif velocity.x < 0: dir = "Left"
+		
+		animation.play("walk" + dir)
+		
+	else: animation.play("idle"+dir)
+func jump(delta):
+	if not is_on_floor(): velocity.y += gravity * delta
+	
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+func walk():
 	var direction = Input.get_axis("ui_left", "ui_right")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
+		animation.play("idle"+dir)
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
+func _physics_process(delta):
+	update_animation()
+	walk()
+	jump(delta)
 	move_and_slide()
+	
