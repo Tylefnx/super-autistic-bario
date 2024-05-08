ed1c17daf8ae6876885cd1d154d24b67a1b65d9a
Tylefnx <tayfunucuncu@gmail.com>
2024-05-08 22:38:41 +0300

    Fixed zombie node type

--- scripts/characters/zombie.gd
+++ scripts/characters/zombie.gd
@@ -1,13 +1,10 @@
 extends Node2D
 
-@onready var ray_cast_right = $ray_cast_right
-@onready var ray_cast_left = $ray_cast_left
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
