f73bf966e5c4b26831f550113fe23f5b981c3938
Tylefnx <tayfunucuncu@gmail.com>
2024-05-10 03:41:41 +0300

    Enemy animations work

--- scripts/characters/zombie.gd
+++ scripts/characters/zombie.gd
@@ -2,10 +2,19 @@ extends CharacterBody2D
 
 @onready var raycast_left = $raycast_left
 @onready var raycast_right = $raycast_right
-
+@onready var animation = $AnimatedSprite2D
+var dir = "Left"
 const speed = 30.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var direction = Vector2.RIGHT
+
+func update_animations():
+	if velocity.length() == 0:
+		animation.play("idle"+dir)
+	else:
+		if velocity.x > 0: dir = "Right"
+		elif velocity.x < 0: dir = "Left"
+		animation.play("walk"+dir)
 
 func change_direction():
 	if raycast_left.is_colliding():
@@ -14,6 +23,7 @@ func change_direction():
 		direction = Vector2.LEFT
 
 func _physics_process(delta):
+	update_animations()
 	change_direction()
 	if not is_on_floor():
 		velocity.y += gravity * delta
