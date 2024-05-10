cd627cadf2fa91b80e3f56bfc29bd57c00f77178
Tylefnx <tayfunucuncu@gmail.com>
2024-05-11 01:06:35 +0300

    Muscle man slam

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -8,6 +8,7 @@ extends Node2D
 @onready var dialogue_timer = $CanvasLayer/Panel/dialogue_timer
 @onready var cutscene = $CanvasLayer/Panel/Label/AnimationPlayer
 @onready var musc_anim = $muscle_man/AnimatedSprite2D
+@onready var run_timer = $muscle_man/run_timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -40,6 +41,9 @@ func dialogue():
 	cutscene.play("muscle_man3")
 	musc_anim.play("transformation")
 	await dialogue_timer.timeout
+	run_timer.start()
+	musc_anim.play("running")
+	await run_timer.timeout
 	dialogue_timer.start()
 	cutscene.play("bario4")
 	await dialogue_timer.timeout
@@ -60,3 +64,7 @@ func _on_area_2d_body_exited(body):
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	#if event == Input.is_action_just_pressed("interact"):
 	print("Cutscene")
+
+
+func _on_run_timer_timeout():
+	musc_anim.stop()
