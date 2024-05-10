06c0ed581549ec267bb7bac51573da4e63702731
Tylefnx <tayfunucuncu@gmail.com>
2024-05-10 03:32:42 +0300

    Enemy moves !

--- scripts/characters/zombie.gd
+++ scripts/characters/zombie.gd
@@ -1,10 +1,23 @@
-extends Node2D
+extends CharacterBody2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@onready var raycast_left = $raycast_left
+@onready var raycast_right = $raycast_right
 
+const speed = 30.0
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var direction = Vector2.RIGHT
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+func change_direction():
+	if raycast_left.is_colliding():
+		direction = Vector2.RIGHT
+	elif raycast_left.is_colliding():
+		direction = Vector2.LEFT
+
+func _physics_process(delta):
+	change_direction()
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	else:
+		velocity = speed * direction
+
+	move_and_slide()
