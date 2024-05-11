e483785fd7b4e5793f22866a65b83cf3f0beb9eb
Tylefnx <tayfunucuncu@gmail.com>
2024-05-11 15:22:07 +0300

    Useless platform implemented successfully

--- scripts/levels/stage_1.gd
+++ scripts/levels/stage_1.gd
@@ -4,6 +4,9 @@ extends Node2D
 func _ready():
 	pass # Replace with function body.
 @onready var bario = $bario
+@onready var panel = $hud/Control/Panel
+@onready var dialog = $hud/Dialog
+@onready var useless_timer = $grass_platform/uselesness/useless_timer
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,6 +15,15 @@ func _process(delta):
 
 
 func _on_end_stage_body_entered(body):
-	if bario.chromosomes == 83:
-		bario.update_max_health()
-	pass #get_tree().change_scene_to_file("res://scenes/levels/boss_1.tscn")
+	if body.name == "bario":
+		if bario.chromosomes == 83:
+			bario.update_max_health()
+		pass #get_tree().change_scene_to_file("res://scenes/levels/boss_1.tscn")
+
+
+func _on_uselesness_body_entered(body):
+	dialog.panel_visible(true)
+	dialog.set_label("Useless Platform:\nEverybody calls me useless for the way I created...\nYou won't call me that way will you :((((")
+	useless_timer.start()
+	await useless_timer.timeout
+	dialog.panel_visible(false)
